4704bb1c2c30e1b2268c69b68765cd3be616fd2b
Belenkov Sergey <s.belenkov@securitycode.ru>
2021-08-03 14:04:05 +0300

    progress: support flexible pattern

--- progress.sh
+++ progress.sh
@@ -2,40 +2,47 @@
 
 # Shows progress of any number of any amount
 # Usage:
-#  progress [in comment] is portion [of content] [as filling] [on breadth]
+#  progress [in comment] is portion [of summary] [as pattern] [on breadth]
 # Where:
-#  @comment: a description of what is going on
-#  @portion: a number of completed parts of content or percentage (X%)
-#  @content: a number of total amount
-#  @filling: a character or a string for filling a progress bar
-#  @breadth: a width of progress bar
+#  @comment: a description of what is going on (default: progress)
+#  @portion: a number of completed parts of total amount or percentage (X%)
+#  @summary: a number of total amount (required if portion is not percentage)
+#  @pattern: a format string of progress bar (default: '[# ]'), where:
+#   - 1st char: opening brace of progress bar
+#   - 2nd char: filling char of progress bar
+#   - 3rd char: spacing char of progress bar
+#   - 4th char: closing brace of progress bar
+#  @breadth: a width of progress bar (default: 100)
 # Examples:
 #  progress in scanning is 50 of 100
 #  progress in watching is 70%
 #  progress in training is 7 of 13
-#  progress in sleeping is 3 of 10 as '#' on 100
+#  progress in sleeping is 3 of 10 as '(# )' on 100
 progress() {
-	local comment portion=0 content=100
-	local percent=0 breadth=80 filling=.
+	local comment=progress
+	local portion=0 summary=0
+	local breadth=100 pattern='[# ]'
 	while (( "$#" )); do case $1 in
 		in) comment=$2; shift 2;;
 		is) portion=$2; shift 2;;
-		of) content=$2; shift 2;;
-		as) filling=$2; shift 2;;
+		of) summary=$2; shift 2;;
+		as) pattern=$2; shift 2;;
 		on) breadth=$2; shift 2;;
 		*) shift 1;;
 	esac done
-	if [[ ! $portion =~ ^[0-9]{1,2}%$ ]]; then
-		percent=$((( $portion * 100 / $content )))
-	else
-		percent=${portion%\%}
+	if [[ $portion =~ ^[0-9]+%$ ]]; then
+		portion=${portion%\%}; summary=100
+	fi
+	if [[ $portion -gt $summary || $summary -eq 0 ]]; then
+		echo -en "\rInvalid progress: $portion of $summary"
+		return 1
 	fi
 
-	local display
-	printf -v display "%*s" "$(( $percent * $breadth / 100 ))" ""
-	display=${display// /$filling}
-	printf "\r%s [%-*s] %3d%%" "$comment" "$breadth" "$display" "$percent"
-	if [[ $percent -eq 100 ]]; then
-		echo
-	fi
+	local percent=$(( $portion * 100 / $summary ))
+	local filling=$(( $percent * $breadth / 100 ))
+	local spacing=$(( $breadth - $filling))
+	printf -v filling '%*s' $filling; filling=${filling// /${pattern:1:1}}
+	printf -v spacing '%*s' $spacing; spacing=${spacing// /${pattern:2:1}}
+	printf "\r%s ${pattern:0:1}%s%s${pattern:3:1} %3d%%" \
+		"$comment" "$filling" "$spacing" "$percent"
 }
--- test/test-progress.sh
+++ test/test-progress.sh
@@ -2,15 +2,29 @@
 
 source $(dirname $0)/../progress.sh
 
-timer=10
-for (( sec=1; sec <= $timer; sec++ )); do
-    progress in "sleeping" is $sec of $timer as '#' on 80
-    sleep 1
+# show forward progress with different patterns
+for pattern in '[#_]' '(!.)' '<+->' '{| }' '|> |' ; do
+	for (( percent = 0; percent <= 100; percent++ )); do
+		progress in sleeping is $percent% as "$pattern" on 100
+		sleep 0.05
+	done
+	echo
 done
 
-progress in scanning is 50 of 100
+# show backward progress (e.g. uninstalling)
+for (( percent = 100; percent >= 0; percent-- )); do
+	progress in uninstalling is $percent% as '>= <' on 96
+	sleep 0.1
+done
 echo
-progress in watching is 70%
+
+progress in scanning is 170 of 360
 echo
-progress in training is 7 of 13 as '*'
+progress in watching is 60 of 240
+echo
+progress in training is 7 of 13 as '(#_)' on 50
+echo
+progress is 100 of 50
+echo
+progress is 1000%
 echo
