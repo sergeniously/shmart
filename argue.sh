f5621224a8017e4e1d035175c10d880839486775
Sergey Belenkov <sergeniously@mail.ru>
2021-08-15 21:27:08 +0300

    argue: fix storing multiple arguments

--- argue.sh
+++ argue.sh
@@ -2,46 +2,61 @@
 
 # Parse, enter or print arguments like argument[=value];
 # Usage:
-#  argue required|optional argname [...] [to varname [~ pattern |= certain] [or default] [of measure]] [do command] [as comment] -- $@
-#   @argname: a name pattern of an argument, e.g. "-a|--arg"
-#   @varname: a name of a variable a value will be stored to
-#   @pattern: a regular expression a value will be validated by
-#   @certain: a certain value which will be stored if an argument is present
-#   @default: a default value which will be stored if an argument is not present
-#   @measure: an unit of an argument value
-#   @command: a command which will be performed if an argument is present
+#  argue required|optional argname[...] [to varname[[]] [~ pattern |= certain] [or default] [of measure]] [do command] [as comment] -- $@
+#   @argname: a pattern of an argument name, e.g. "-a|--arg"
+#    * adding ... at the end of it makes an argument multiple
+#   @varname: a name of a variable to store a value
+#    * adding [] at the end of it tells to consider a variable as an array
+#   @pattern: a regular expression to validate a value
+#   @certain: a certain value which will be stored if an argument is specified
+#    * it is used only if a  validation pattern is not specified
+#   @default: a default value which will be stored if an argument is not specified
+#   @measure: a unit of an argument value
+#    * set it as PASSWORD to mask a value with asterisks on input
+#   @command: a command which will be performed if an argument is specified
 #   @comment: a description of an argument
-#   '...': tells that argument is multiple
 # Examples:
 #  argue required --username of USERNAME to username ~ "[a-zA-Z0-9_]{3,16}" as 'Make up a username' -- $@
 #  argue required --password of PASSWORD to password ~ ".{6,32}" as 'Make up a password' -- $@
 #  argue optional --gender to gender ~ "(male|female)" or 'unknown' as 'How do you identify yourself?' -- $@
-#  argue optional --language ... of LANGUAGE to languages ~ "[a-z]+" as 'Which laguages do you speak?' -- $@
+#  argue optional --language... of LANGUAGE to languages[] ~ "[a-z]+" as 'Which laguages do you speak?' -- $@
 #  argue optional --robot to robot = yes or no as 'Are you a robot?' -- $@
 argue() {
-	local _meaning _argname _varname _measure _several
+	local _meaning _argname _several _varname _vartype _measure
 	local _pattern _certain _default _command _comment
 	while (( "$#" )); do case $1 in
 		optional|required)
 			_meaning=$1
-			_argname=$2; shift 2;;
-		of) _measure=$2; shift 2;;
-		as) _comment=$2; shift 2;;
-		to) _varname=$2; shift 2;;
+			_argname=${2%...}
+			_several=${2#$_argname}
+			shift 2;;
+		to) _varname=${2%[]}
+			_vartype=${2#$_varname}
+			shift 2;;
 		 ~) _pattern=$2; shift 2;;
 		 =) _certain=$2; shift 2;;
+		of) _measure=$2; shift 2;;
 		or) _default=$2; shift 2;;
 		do) _command=$2; shift 2;;
-		...) _several=$1; shift;;
+		as) _comment=$2; shift 2;;
 		--) shift; break;;
-		 *) echo "Argue: invalid parsing option $1"; exit 1;;
+		 *) echo "argue: invalid parsing option $1"; exit 1;;
 	esac done
 	# print argument
-	if [[ $1 =~ ^(-h|--help|help)$ && ! $1 =~ ^($_argname)$ ]]; then
+	if [[ $1 =~ ^(-h|--help|help)$ ]]; then
+		[[ $1 =~ ^($_argname)$ && -n $_command ]] && eval "$_command"
 		printf "%2s${_argname//|/, }${_pattern+=${_measure-$_pattern}${_several}${_default+ (default: '$_default')}}\n"
 		printf "%6s*${_meaning}* ${_comment}\n"
 		return 202 # aka sos
 	fi
+
+	argue_store() {
+		if ((${#1})); then
+			eval $(printf $([[ $_vartype == '[]' ]] && echo "%s+=('%s')" || echo "%s='%s'") "$_varname" "$1")
+		elif [[ ! -v $_varname ]]; then
+			eval $(printf $([[ $_vartype == '[]' ]] && echo "%s=()" || echo "%s=''") "$_varname")
+		fi
+	}
 	# enter argument
 	if !(("$#")) && [[ -n $_varname ]]; then
 		local _consent="y|yes" _dissent="n|no"
@@ -63,13 +78,13 @@ argue() {
 			if [[ -n $_entered ]]; then
 				[[ ! $_entered =~ ^$_ex_pattern$ ]] && echo " # invalid value; expected $_ex_pattern" && continue
 				if [[ -z $_pattern ]]; then
-					declare -g "$_varname=$([[ $_entered =~ ^($_consent)$ ]] && echo $_certain || echo $_default)"
+					argue_store $([[ $_entered =~ ^($_consent)$ ]] && echo "$_certain" || echo "$_default")
 				else
-					declare -g "$_varname=$_entered"
+					argue_store "$_entered"
 				fi
 			elif [[ $_counter -eq 0 ]]; then
 				[[ $_meaning == required ]] && echo "# empty value of required argument" && continue
-				declare -g "$_varname=$_default"
+				argue_store "$_default"
 			fi
 			(( _counter++ )); _meaning=optional; echo
 			[[ -z $_entered || -z $_several ]] && break
@@ -86,9 +101,9 @@ argue() {
 				if [[ -n $_pattern ]]; then
 					[[ ! $1 =~ ^.+=.+$ ]] && echo "Error: missed value for argument '$1'" && exit 1
 					[[ ! ${1#*=} =~ ^$_pattern$ ]] && echo "Error: invalid value of argument '$1'; expected $_pattern" && exit 1
-					[[ -n $_several ]] && eval "$_varname+=('${1#*=}')" || declare -g "$_varname=${1#*=}"
+					argue_store "${1#*=}"
 				else
-					[[ -n $_several ]] && eval "$_varname+=('${_certain-$1}')" || declare -g "$_varname=${_certain-$1}"
+					argue_store "${_certain-$1}"
 				fi
 			fi
 			(( _counter++ ))
@@ -97,7 +112,7 @@ argue() {
 	done
 	if !(( $_counter )); then
 		[[ $_meaning == required ]] && echo "Error: missed required argument ${_argname//|/, }" && exit 1
-		[[ -n $_varname ]] && declare -g "$_varname=$_default" && return 0
+		[[ -n $_varname ]] && argue_store "$_default" && return 0
 		return 1
 	else
 		[[ -n $_command ]] && eval "$_command"
--- test/argue.sh
+++ test/argue.sh
@@ -2,34 +2,42 @@
 
 source $(dirname $0)/../argue.sh
 
-argue optional "-h|--help|help" do 'echo Options:' \
-    as 'Print this usage' -- $@
-argue required --language ... of LANGUAGE to languages ~ "[a-z]+" \
-    as 'Which languages do you speak?' -- $@
-argue optional --udp to protocols = UDP \
-    as 'Use UDP protocol?' -- $@
-argue optional --tcp to protocols = TCP \
-    as 'Use TCP protocol?' -- $@
+usage() {
+    echo 'About: test script for argue function'
+    echo "Usage: $(basename $0) [options]"
+    echo " * run without options to input them"
+    echo 'Options:'
+}
+
+argue optional "-h|--help|help" do usage \
+	as 'Print this usage' -- $@
 argue required --username of USERNAME to username ~ "[a-zA-Z0-9_]{3,16}" \
-    as 'Make up a username' -- $@
+	as 'Make up a username' -- $@
 argue required --password of PASSWORD to password ~ ".{6,32}" \
-    as 'Make up a password' -- $@
+	as 'Make up a password' -- $@
 argue optional --realname of STRING to realname ~ "[[:alnum:]\ ]{3,32}" or "$username" \
-    as 'What is your real name?' -- $@
+	as 'What is your real name?' -- $@
 argue required --age of NUMBER to age ~ "[1-9][0-9]{0,2}" \
-    as 'How old are you?' -- $@
+	as 'How old are you?' -- $@
 argue optional --gender to gender ~ "(male|female)" or 'unknown' \
-    as 'How do you identify yourself?' -- $@
+	as 'How do you identify yourself?' -- $@
 argue optional --show-password to show_password = yes or no \
-    as 'Do you wanna see password?' -- $@
+	as 'Do you wanna see password?' -- $@
+argue required --language... of LANGUAGE to languages[] ~ "[a-z]+" \
+	as 'Which languages do you speak?' -- $@
+argue optional --books to interests[] = books \
+	as 'Do you like reading books?' -- $@
+argue optional --music to interests[] = music \
+	as 'Do you like listening music?' -- $@
 [[ $? -eq 202 ]] && exit
 
 echo
 echo "Your registration info"
 printf "%10s: %s\n" \
-    'Username' $username \
-    'Password' "$([[ $show_password == yes ]] && echo "$password" || echo "${password//?/*}")" \
-    'Real name' "$realname" \
-    'Age' $age \
-    'Gender' $gender \
-    'Languages' "${languages[*]}"
+	'Username' "$username" \
+	'Password' "$([[ $show_password == yes ]] && echo "$password" || echo "${password//?/*}")" \
+	'Real name' "$realname" \
+	'Age' "$age" \
+	'Gender' "$gender" \
+	"Languages" "${languages[*]}" \
+    "Interests" "${interests[*]}"
