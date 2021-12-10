4746ba9b079d25e45463b269db719de1c19d155e
Belenkov Sergey <s.belenkov@securitycode.ru>
2021-12-10 17:59:22 +0300

    add versions comparator script

---
+++ core/compver.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# About:
+#  compare any kinds of versions
+#  * splits version strings to number arrays deleting non-digit characters
+#  * checks numbers of one array against numbers of another array
+#  * returns 0 if condition succeeds, otherwise 1
+# Usage:
+#  compver <ver1> <greater|less|equal|with> <ver2>
+# Examples:
+#  compare_version 4.20.206-2 greater 4.19.206-2 # return 0 (true)
+#  compare_version 1.2.3-4 less 1.2.3 # return 1 (false)
+#  compare_version 4.0-RC2 equal 4.0-RC1 # return 1 (false)
+#  compare_version 3.04.2b with 3.04.3 # echo less
+compver() {
+    local left=(${1//[^[:digit:]]/ })
+    local right=(${3//[^[:digit:]]/ })
+    local comparison=$2 conclusion=equal
+    local number count=$((${#left[@]} > ${#right[@]} ? ${#left[@]} : ${#right[@]}))
+    for ((number = 0; number < count; number++)); do
+        if [[ ${left[$number]} -gt ${right[$number]} ]]; then
+            conclusion=greater; break
+        elif [[ ${left[$number]} -lt ${right[$number]} ]]; then
+            conclusion=less; break
+        fi
+    done
+    [[ $comparison == with ]] && echo $conclusion || \
+        test $conclusion == $comparison
+}
---
+++ core/test/compver.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source $(dirname $0)/../compver.sh
+
+# 'ver1 ver2 expected'
+test_cases=(
+    '1.2.3-1 1.2.3 greater'
+    '1.2.3 1.2.3-1 less'
+    '1.2.3-1 1.2.3-1 equal'
+
+    '4.20.206-2 4.19.206-2 greater'
+    '4.19.206-2 4.20.206-2 less'
+    '4.19.206-2 4.19.206-2 equal'
+
+    '4.0-RC2 4.0-RC1 greater'
+    '4.0-RC1 4.0-RC2 less'
+    '4.0-RC1 4.0-RC1 equal'
+
+    '3.002 3.003.3 less'
+    '3.003.3 3.002 greater'
+    '3.003.3 3.003.3 equal'
+
+    '3.04.2b 3.04.3 less'
+    '3.04.3 3.04.2b greater'
+    '3.04.2b 3.04.2b equal'
+)
+
+for test_case in "${test_cases[@]}"; do
+    given=($test_case)
+    version1=${given[0]}
+    version2=${given[1]}
+    expected=${given[2]}
+    received=$(compver $version1 with $version2)
+    [[ $received == $expected ]] && echo -n "[SUCCESS] " || echo -n "[FAILURE] "
+    echo "compver $version1 with $version2: $received == $expected ?"
+done
