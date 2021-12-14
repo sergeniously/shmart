5b3561b25094d3000c7dc4b7cce6e4232d9af7bd
Sergey Belenkov <sergeniously@mail.ru>
2021-12-15 01:07:13 +0300

    add git-review script for pushing commits

---
+++ core/git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+	echo "It seems to be not a git repository (or any of the parent directories)" > /dev/stderr
+	exit 1
+fi
+
+GIT_BRANCH=`git rev-parse --abbrev-ref HEAD 2> /dev/null`
+if [[ $GIT_BRANCH =~ ^(.*)$ ]]; then
+	GIT_BRANCH=${BASH_REMATCH[1]}
+fi
+
+GIT_ROOT=`git rev-parse --show-toplevel 2> /dev/null`
